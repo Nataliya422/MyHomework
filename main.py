ab91c76e8c31b0b8be8072aaafe1bc96d46cea78
Nataliya422 <natasavkina310709@yandex.ru>
2024-08-29 23:00:16 +0700

    ДЗ №4 - игра в города

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
+
 from data.cities import*
+import _json
 # Собираем set всех городов в нижнем регистре
 cities_set = {city["name"].lower() for city in cities_list}
 
@@ -20,3 +23,21 @@ print(iter_count)
 
 all_letter = set(str(cities_set))
 print(all_letter)
+
+
+
+print(cities_list)
+
+#with open("cities.json", "w", encoding="utf-8") as file:
+#    json.dump(cities_list, file, ensure_ascii=False, indent=4)
+
+#with open("cities.json", "r", encoding="utf-8") as file:
+#    cities_list = json.load(file)
+
+cities_set = {city['name'] for city in cities_list}
+print(cities_set)
+
+#while True:
+    #Ход человека
+    #Ввод города
+    #human_city = input("Введите город:")
