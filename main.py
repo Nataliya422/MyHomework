1c4190d9f332abfc3c4c80afbe724b4c7bb458db
Nataliya422 <natasavkina310709@yandex.ru>
2024-09-02 00:26:20 +0700

    дописала правильно код по ДЗ

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
 import json
 
-from data.cities import*
+from data.cities import *
 import _json
-# Собираем set всех городов в нижнем регистре
-cities_set = {city["name"].lower() for city in cities_list}
-
-# Цикл в цикле
-iter_count = 0
-bad_letters = set()
-for city in cities_set:
-    last_letter = city[-1]
-    for city_2 in cities_set:
-        first_letter = city_2[0]
-        iter_count += 1
-        if last_letter == first_letter:
-            break
-    else:
-        bad_letters.add(last_letter)
-
-print(bad_letters)
-print(iter_count)
-
-all_letter = set(str(cities_set))
-print(all_letter)
-
-
-
-print(cities_list)
 
 #with open("cities.json", "w", encoding="utf-8") as file:
 #    json.dump(cities_list, file, ensure_ascii=False, indent=4)
@@ -34,10 +9,48 @@ print(cities_list)
 #with open("cities.json", "r", encoding="utf-8") as file:
 #    cities_list = json.load(file)
 
+#print(cities_list)
+
 cities_set = {city['name'] for city in cities_list}
 print(cities_set)
 
-#while True:
+computer_city = ''
+
+while True:
     #Ход человека
+
     #Ввод города
-    #human_city = input("Введите город:")
+    human_city = input("Введите город:")
+
+    #проверка наличия города в списке
+
+    if human_city.lower() in {city.lower() for city in cities_set}:
+        print(f"Город {human_city} есть в списке.")
+    else:
+        print(f"Города {human_city} нет в списке. Человек проиграл")
+        break
+
+    #проверка, а бы ход компьютеру
+
+    if computer_city:
+        if human_city[0].lower() == computer_city[-1].lower():
+            print("Условия выполнены, ход компьютеру.")
+
+        else:
+            print(f"Условия не выполнены, город должен начинаться с {computer_city[-1].upper()}")
+            break
+
+    #ход компьютеру
+
+    #перебор сета с городами, подбор подходящего города по первой букве
+
+    for city in cities_set:
+        if human_city[-1].lower() == city[0].lower():
+            computer_city = city
+            print(f"Компьютер выбрал город {computer_city}")
+            break
+    else:
+         #если в цикле не сработал break мы попадем сюда
+        print("Компьютер не смог выбрать город")
+        break
+
